e6b6778fa1533453b8ed8f3ba86def25b21209f2
Vincent Gillet <vincent.gillet@zenika.com>
2020-06-09 11:13:21 +0200

    bug fix projectID added instead of name

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@
 variable "project" {
   description = "The project ID where all resources will be launched."
   type        = string
-  default = "lab-gitops"
+  default = "lab-gitops-279807"
 }
 
 variable "location" {
